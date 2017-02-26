ce3d34c3351a437ac69329f73c5786bad1b648fd
ZhangBoshu <zhangboshu2004@163.com>
2017-02-26 21:40:50 +0800

    初始化

--- app/src/main/java/com/zhangboshu/demo/base/BaseActivity.java
+++ app/src/main/java/com/zhangboshu/demo/base/BaseActivity.java
@@ -1,13 +1,12 @@
-package com.zhangboshu.demo;
+package com.zhangboshu.demo.base;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/zhangboshu/demo/ui/MainActivity.java
@@ -0,0 +1,38 @@
+package com.zhangboshu.demo.ui;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.zhangboshu.demo.R;
+import com.zhangboshu.demo.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+    }
+}
